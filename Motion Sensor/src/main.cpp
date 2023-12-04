e354075c8ba0846649a304980941973886934c30
LJMU-JackMallo <csmjmal1@ljmu.ac.uk>
2023-12-04 16:00:26 +0000

    version without delay
    Needs tidying up abit but it works, need to figure out minimum delay time on sensor, but works well at the moment if you wait a couple of seconds in between triggering the sensor.

--- Motion Sensor/src/main.cpp
+++ Motion Sensor/src/main.cpp
@@ -1,35 +1,77 @@
 #include <Arduino.h>
 
-// Variable for Motion Detected
-int motionDetected = 0;
  
 // Variable to store value from PIR
 int pirValue; 
+bool motionDetected;
 
+unsigned long lastChangeTime;
+const int DELAY = 2000;
 
 int laserPin = 11;
 int pirPin = 7;
 
+enum State{
+  LASER_ON,
+  LASER_OFF
+};
+
+State current;
+
+boolean timeDiff(unsigned long start, int specifiedDelay) {
+  return (millis() - start >= specifiedDelay);
+}
+
 void setup() {
+
+  current = LASER_OFF;
+  lastChangeTime=0;
   pinMode(laserPin, OUTPUT);
   pinMode(pirPin, INPUT);
   Serial.begin(9600);
-  // delay(60000);
+
+  // Wait for one minute to properly initialise sensor
+  Serial.println("Preparing sensor.....");
+  //delay(60000);
+  Serial.println("Ready to go!");
 }
+
+
 
 void loop() {
   
+  State old = current;
+
   // Get value from motion sensor
-	pirValue = digitalRead(pirPin);
-	// See if motion Detected
-	if (pirValue == 1){
-		// turn laser on for 3 seconds
-		digitalWrite(laserPin, HIGH);
-		Serial.print("Motion Detected!");
-		delay(5000);
-    digitalWrite(laserPin, LOW); // Turn off the laser head
-    delay(5000); 
+  if (digitalRead(pirPin) == HIGH){
+    motionDetected = true;
+  }
+  else {
+    motionDetected = false;
+  }
+
+	// See if motion detected 
+	if (motionDetected && timeDiff(lastChangeTime,DELAY)) {
+
+    switch(current){
+      case LASER_OFF:
+        Serial.println("Turning light on!");
+        digitalWrite(laserPin, HIGH);
+        current = LASER_ON;
+        
+        break;
+
+      case LASER_ON:
+         Serial.println("Turning light off!");
+         digitalWrite(laserPin, LOW); // Turn off the laser 
+         current = LASER_OFF;
+         
+         break;
+    }  
+  }
+
+  if (old != current){
+    lastChangeTime = millis();
   }
     
-
 }
